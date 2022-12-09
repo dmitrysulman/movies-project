4fdb4f3997677da93af1f02cac45aeebdc57a13f
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-09 18:28:02 +0200

    c1

---
+++ src/components/Movie.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Movie() {
+    return <div className="movie card"></div>;
+}
+
+export { Movie };
---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+import { Movies } from "../components/Movies"
+
+function Main() {
+    return <main className="container content">
+        <Movies />
+    </main>
+}
+
+export {Main}
