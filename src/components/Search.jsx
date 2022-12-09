0a5f8fd47cd398624d795ee23f3c6f2d6e79c888
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-09 23:11:05 +0200

    3

---
+++ src/components/Search.jsx
@@ -0,0 +1,78 @@
+import { Component } from "react";
+
+class Search extends Component {
+    state = {
+        search: "",
+        type: "",
+    }
+
+    handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            this.props.searchCallBack(this.state.search, this.state.type);
+        }
+    }
+
+    handleRadiobuttonChange = (event) => {
+        this.setState({type: event.target.value});
+    }
+
+    render() {
+        const { search, type } = this.state;
+
+        return <div className="row">
+            <div className="input-field">
+                <input
+                    className="validate"
+                    type="search"
+                    placeholder="search"
+                    value={search}
+                    onChange={(e) => this.setState({ search: e.target.value })}
+                    onKeyDown={this.handleKeyDown}
+                />
+                <button
+                    className="btn search-btn"
+                    onClick={() => this.props.searchCallBack(this.state.search, this.state.type)}
+                >
+                    Submit
+                </button>
+            </div>
+            <p>
+                <label>
+                    <input
+                        className="with-gap"
+                        name="type"
+                        value=""
+                        type="radio"
+                        onChange={this.handleRadiobuttonChange}
+                        checked={type === ""}
+                    />
+                    <span>All</span>
+                </label>
+                <label>
+                    <input
+                        className="with-gap"
+                        name="type"
+                        value="movie"
+                        type="radio"
+                        onChange={this.handleRadiobuttonChange}
+                        checked={type === "movie"}
+                    />
+                    <span>Movie</span>
+                </label>
+                <label>
+                    <input
+                        className="with-gap"
+                        name="type"
+                        value="series"
+                        type="radio"
+                        onChange={this.handleRadiobuttonChange}
+                        checked={type === "series"}
+                    />
+                    <span>Series</span>
+                </label>
+            </p>
+        </div>
+    }
+}
+
+export { Search }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,27 +1,34 @@
 import { Component } from "react"
 import { Movies } from "../components/Movies"
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
 
 class Main extends Component {
-
     state = {
         movies: [],
     }
 
-
     componentDidMount() {
-        fetch("http://www.omdbapi.com/?apikey=c9289430&s=matrix")
+        this.fetchMovies("matrix", "");
+    }
+
+    fetchMovies = (text, type) => {
+        fetch(`http://www.omdbapi.com/?apikey=c9289430&s=${text}&type=${type}`)
             .then(response => response.json())
             .then(data => this.setState({ movies: data.Search }));
     }
 
     render() {
-        const {movies} = this.state;
+        const { movies } = this.state;
 
         return <main className="container content">
+            <Search searchCallBack={this.fetchMovies} />
             {
                 movies.length ? (
                     <Movies movies={movies} />
-                ) : <h3>Loading...</h3>
+                ) : (
+                    <Preloader />
+                )
             }
         </main>
     }
